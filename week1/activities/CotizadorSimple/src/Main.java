abe590ecce397037272882d86cab54e56a75a400
Cristian Penagos <suarezalejo26@gmail.com>
2025-09-08 14:05:30 -0500

    Finish cotizador

---
+++ week1/activities/CotizadorSimple/src/Main.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("------- FitPro Gym - Quoter-------");
+
+        String nameUser;
+        byte age;
+        double height;
+        double weight;
+        int answerSubscription;
+        String subscription;
+        String answerFirstTime;
+        boolean firstTime;
+        int discount1 = 0, discount2 = 0;
+        double Baseprice = 0;
+
+        System.out.println("Is your first time in the Gym? (y/n)");
+        answerFirstTime = sc.next().toLowerCase();
+        if (answerFirstTime.equals("y")){
+            firstTime = true;
+            discount1 = 10;
+        } else if (answerFirstTime.equals("n")){
+            firstTime = false;
+        }
+
+        System.out.println("\nEnter your name: ");
+        nameUser = sc.next().toUpperCase();
+
+        System.out.println("\nEnter your age: ");
+        age = sc.nextByte();
+
+        if (age>=16 && age<=25){
+            discount2 = 10;
+        }
+
+        System.out.println("\nEnter your height (meters): ");
+        height = sc.nextDouble();
+
+        System.out.println("\nEnter your weight (kg): ");
+        weight = sc.nextDouble();
+
+        System.out.println("\nChoose a number of subscription: \n1. BASIC\n2. PLUS\n3. PREMIUM");
+        answerSubscription = sc.nextInt();
+        if (answerSubscription == 1){
+            subscription = "BASIC";
+            Baseprice = 80;
+        } else if (answerSubscription == 2){
+            subscription = "PLUS";
+            Baseprice = 120;
+        }else {
+            subscription = "PREMIUM";
+            Baseprice = 180;
+        }
+
+        double bmi;
+        String showBMI="";
+
+        bmi = weight/(height*height);
+        if (bmi>=30){
+            showBMI = "Obesity";
+        }
+
+        if (bmi>25 && bmi<29.9){
+            showBMI = "Overweight";
+        }
+
+        if (bmi>18.5 && bmi<24.9){
+            showBMI = "Normal";
+        }
+
+        if (bmi<=18.5){
+            showBMI = "Underweight";
+        }
+
+        double finalPrice;
+        finalPrice = Baseprice-(Baseprice*((double) (discount1+discount2) /100));
+
+        System.out.println("\n------ Result ------");
+        System.out.println("Name: "+nameUser);
+        System.out.println("Subscription chosen: "+subscription);
+        System.out.println("Base price: $"+Baseprice);
+        System.out.println("Discount applied: "+(discount1+discount2)+"%");
+        System.out.println("Final price: $"+finalPrice);
+        System.out.println("BMI category: "+showBMI);
+
+
+    }
+
+}
