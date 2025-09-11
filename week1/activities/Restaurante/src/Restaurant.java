ed5c6a68dc445a72e626ce4e53d034152b59f9ec
Cristian Penagos <suarezalejo26@gmail.com>
2025-09-11 07:18:18 -0500

    new chances

--- week1/activities/Restaurante/src/Restaurant.java
+++ week1/activities/Restaurante/src/Restaurant.java
@@ -18,8 +18,8 @@ public class Restaurant {
 
         do {
             System.out.println("\n--- Restaurant ---");
-            System.out.println("1. Add Order \n2. Show Menu \n3. Check Payment \n4. Show Unpaid \n5. Exit");
-            System.out.print("Choose an option: ");
+            System.out.println("1. Create Order \n2. Show Menu \n3. Check Payment for Orders \n4. Show Unpaid Orders \n5. Exit");
+            System.out.println("\nChoose an option: ");
             optionMenu = sc.nextInt();
 
             switch (optionMenu) {
@@ -29,27 +29,43 @@ public class Restaurant {
                     for (int i = 0; i < menu.size(); i++) {
                         System.out.println((i + 1) + ". " + menu.get(i));
                     }
-                    System.out.println("Enter item number (0 to finish): ");
+                    System.out.println("\nEnter the item number you want to add to the order: ");
 
                     double total = 0.0;
 
                     int optionItem;
+                    optionItem = sc.nextInt();
+                    if (optionItem > 0 && optionItem <= menu.size()) {
+                        String selected = menu.get(optionItem - 1);
+                        order.add(selected);
+                        total += Double.parseDouble(selected.split("\\$")[1]);
+                    } else if (optionItem != 0) {
+                        System.out.print("\nInvalid dish. Try again: ");
+                    }
                     do {
-                        optionItem = sc.nextInt();
-                        if (optionItem > 0 && optionItem <= menu.size()) {
-                            String selected = menu.get(optionItem - 1);
-                            order.add(selected);
-                            total += Double.parseDouble(selected.split("\\$")[1]);
-                        } else if (optionItem != 0) {
-                            System.out.print("Invalid dish. Try again: ");
+
+                        System.out.println("\nDo you want to add another dish? (y/n):");
+                        String answer = sc.next().toLowerCase();
+
+                        if (answer.equals("y")) {
+                            System.out.println("\nEnter the item number you want to add to the order: ");
+                            optionItem = sc.nextInt();
+                            if (optionItem > 0 && optionItem <= menu.size()) {
+                                String selected = menu.get(optionItem - 1);
+                                order.add(selected);
+                                total += Double.parseDouble(selected.split("\\$")[1]);
+                            } else if (optionItem != 0) {
+                                System.out.print("\nInvalid dish. Try again: ");
+                            }
+                        }else {
+                            optionItem = 0;
                         }
                     } while (optionItem != 0);
 
-                    order.add("Total: $" + total);
-                    order.add("Paid: No");
+                    order.add("Total Order: $" + total);
+                    order.add("Order Paid: No");
                     orders.put(customerId, order);
-                    System.out.println("Order added for Customer " + customerId);
-                    System.out.println(orders);
+                    System.out.println("\nOrder created for Customer " + customerId);
                     customerId++;
                     break;
 
@@ -62,37 +78,44 @@ public class Restaurant {
 
                 case 3:
                     System.out.println("-------- Check Payment --------");
-                    System.out.print("Enter Customer ID: ");
-                    int id = sc.nextInt();
 
-                    if (orders.containsKey(id)) {
-                        ArrayList<String> details = orders.get(id);
-                        for (String detail : details) {
-                            System.out.println(detail);
+                    for (Integer idCustomer : orders.keySet()){
+
+                            System.out.println("Customer Id "+idCustomer);
+                            System.out.print("\nEnter Customer Id: ");
+                            int id = sc.nextInt();
+
+                        System.out.println("--------------------");
+
+                            if (orders.containsKey(id)) {
+                                ArrayList<String> details = orders.get(id);
+                                for (String detail : details) {
+                                    System.out.println(detail);
+                                }
+
+                                String paid = details.get(details.size() - 1);
+
+                                if (paid.equals("Order Paid: No")) {
+                                    System.out.println("--------------------");
+                                    System.out.print("Mark as paid? (y/n): ");
+                                    String answer = sc.next().toLowerCase();
+
+                                    if (answer.equals("y")) {
+                                        details.set(details.size() - 1, "Order Paid: Yes");
+                                        System.out.println("\nPayment confirmed.");
+                                    }
+                                }
+                            break;
+                        }else {
+                            System.out.println("No Orders");
+                            break;
                         }
-
-                        String paid = details.get(details.size() - 1);
-
-                        if (paid.equals("Paid: No")) {
-
-                            System.out.print("Mark as paid? (y/n): ");
-                            String answer = sc.next().toLowerCase();
-
-                            if (answer.equals("y")) {
-                                details.set(details.size() - 1, "Paid: Yes");
-                                System.out.println("Payment confirmed.");
-                            }
-                        }
-                    } else {
-                        System.out.println("Customer not found.");
                     }
-                    break;
-
                 case 4:
-                    System.out.println("-------- Unpaid Orders--------");
+                    System.out.println("-------- Unpaid Orders --------");
                     for (Integer idOrder : orders.keySet()) {
                         String paid = orders.get(idOrder).get(orders.get(idOrder).size() - 1);
-                        if (paid.equals("Paid: No")) {
+                        if (paid.equals("Order Paid: No")) {
                             System.out.println("Customer " + idOrder + ": " + orders.get(idOrder));
                         }
                     }
